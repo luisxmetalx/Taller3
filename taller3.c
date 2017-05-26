8f09ac3a7fdbbb811376463154c043febf6baccc
luisxmetalx <you@example.com>
2017-05-26 16:46:38 -0500

    taller terminado

--- taller3.c
+++ taller3.c
@@ -12,26 +12,40 @@ void codigoMorse (char mensaje[]);
 
 int main (int argc, char *argv[]){
     int llave,tam,i;
+    char *mensaje;
+    size_t bufsize = TAMANO;
+    size_t characters;
+    //Inicializacion del puntero..dandole memoria dinamica
+    mensaje = (char *)malloc(bufsize * sizeof(char));
+    if( mensaje == NULL)
+    {
+        perror("Unable to allocate mensaje");
+        exit(1);
+    }
     printf("\n********************************\n");
     printf("********Cifrado Ciclico*********\n");
     printf("********************************\n");
     printf("\n");
-    //prueba si hay algun argumento enviado por consola
-    /*if ( argc == 1 ) {
-        printf( "No arguments were passed.\n" );
-    } else {
-        printf( "Hay argumentos desde consola" );
-        for(i=0; i<argc;i++){
-            printf("\n %s \n",argv[i]);
-        }
-    }*/
 
-    //funcion a utilizar
-    //le mandamos el parametro argv[argc-1] para capturar lo  le enviamos por consola
-    //parametro argc son en numero de parametros que se han enviado, en un principio sera 1(nombre del ejecutable)
-    
-    cifradoCiclico(argv[argc-1],atoi(argv[argc-2]));
-    printf("\n");
+    //prueba si hay algun argumento enviado por consola
+    if ( argc == 1 ) {
+        printf( "No Hay argumentos desde consola.\n\n" );
+        printf("ingrese el mensaje a Cifrar: ");
+        //captura lo que se escribe un una linea...sirve para capturar los espacios entre palabras.
+        characters = getline(&mensaje,&bufsize,stdin);
+        printf("ingrese el numero de llave: ");
+        scanf("%d",&llave);
+        //funcion a utilizar
+        cifradoCiclico(mensaje,llave);
+    } else {
+        printf( "Hay argumentos desde consola\n\n" );
+        //funcion a utilizar
+        //le mandamos el parametro argv[argc-1] para capturar lo  le enviamos por consola
+        //parametro argc son en numero de parametros que se han enviado, en un principio sera 1(nombre del ejecutable)
+        cifradoCiclico(argv[argc-1],atoi(argv[argc-2]));
+        printf("\n");
+        
+    }
     return 0;
 }
 
@@ -72,7 +86,7 @@ void cifradoCiclico(char frase[], int llave){
             break;
         }
     }
-    printf("Cifrado llave %d: %s\n\n",llave,tmp);
+    printf("\nCifrado llave %d: %s\n",llave,tmp);
     printf("\n");
     strcpy(tmp1,tmp);
     printf("clave morse es: ");
