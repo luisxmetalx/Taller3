09d0a84b991fe00b7c343ee7c96e88cc3bbd37ac
Jonathan <jparrales15@gmail.com>
2017-06-03 11:28:04 -0500

    nuevo

--- include/cifraso.h
+++ include/cifraso.h
@@ -1,3 +1,3 @@
-char* cifradoCiclico(char* mensaje, int llave);
-char* cifradoAutollave(char* mensaje, char* llave);
-char* cifradoContrasena(char* mensaje, char* llave);
+void char* cifradoCiclico(char* mensaje, int llave);
+void char* cifradoAutollave(char* mensaje, char* llave);
+void char* cifradoContrasena(char* mensaje, char* llave);
